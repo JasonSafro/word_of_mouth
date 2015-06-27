bab30488f62c86ba27bcb980f8b11894548bdcea
Jason Safro <jacen6678@yahoo.com>
2015-06-26 22:48:29 -0400

    Buffered against use of undefined functions.

--- docroot/application/views/services_view_html2.php
+++ docroot/application/views/services_view_html2.php
@@ -77,7 +77,9 @@ echo js_asset("jquery.responsivetable.js");
 <script>
 var SITE_URL="<?php echo base_url(); ?>";
 $(document).ready(function() {
-$('table').responsiveTable();
+  if( typeof responsiveTable !== 'undefined' && $.isFunction(responsiveTable) ) {
+    $('table').responsiveTable();
+  }
 });
 </script>
 <!--for us of back to top function-->
@@ -85,40 +87,42 @@ $('table').responsiveTable();
 <script src="<?php echo base_url()?>assets/scripts/jquery.wslide.js" type="text/javascript" charset="utf-8"></script>
 <script type="text/javascript" charset="utf-8">
 			$(document).ready(function(){
-				/*====================== Exemple basique ======================*/
-				$("#parent1").wslide({
-					width: 450,
-					height: 200,
-					duration: 2000,
-					effect: 'easeOutBounce'
-				});
-				/*====================== Exemple 2 ======================*/
-				$("#parent2").wslide({
-					width: '100%',
-					height: 284,
-					//pos: 1,
-					//horiz: true,
-					autoplay: true ,
-					fade: true,
-					//delay: 2000,
-					duration: 2000, 
-					//type: 'sequence',
-					//duration: 1000,
-					//effect: 'easeOutElastic'
-					
-				});
-				
-				
-				
-				/*====================== Exemple 3 ======================*/
-				$("#parent3").wslide({
-					width: 250,
-					height: 300,
-					col: 4,
-					autolink: 'menu3',
-					duration: 4000,
-					effect: 'easeOutExpo'
-				});
+        if( typeof wslide !== 'undefined' && $.isFunction(wslide) ) {
+
+          /*====================== Exemple basique ======================*/
+          $("#parent1").wslide({
+            width: 450,
+            height: 200,
+            duration: 2000,
+            effect: 'easeOutBounce'
+          });
+          /*====================== Exemple 2 ======================*/
+          $("#parent2").wslide({
+            width: '100%',
+            height: 284,
+            //pos: 1,
+            //horiz: true,
+            autoplay: true,
+            fade: true,
+            //delay: 2000,
+            duration: 2000,
+            //type: 'sequence',
+            //duration: 1000,
+            //effect: 'easeOutElastic'
+
+          });
+
+
+          /*====================== Exemple 3 ======================*/
+          $("#parent3").wslide({
+            width: 250,
+            height: 300,
+            col: 4,
+            autolink: 'menu3',
+            duration: 4000,
+            effect: 'easeOutExpo'
+          });
+        }
 			});
 </script>
 <?php
